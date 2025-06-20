5f10e62287029e79568ad80d50616cad575e5769
SamarthNiroli <sniroli137@gmail.com>
2025-06-20 11:55:24 +0530

    Added user authentication functionality using the clerk

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,7 @@ import Footer from "./components/Footer";
 
 const App = () => {
 
-  const isAdminRoute = useLocation().pathname.
-  startsWith("/admin");
+  const isAdminRoute = useLocation().pathname.startsWith("/admin");
   return (
     <>
     <Toaster/>
@@ -20,7 +19,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
-        <Route path="/movies:id" element={<MovieDetails />} />
+        <Route path="/movies/:id" element={<MovieDetails />} />
         <Route path="/movies/:id/:date" element={<SeatLayout />} />
         <Route path="/my-bookings" element={<MyBookings />} />
         <Route path="/favorite" element={<Favourite />} />
